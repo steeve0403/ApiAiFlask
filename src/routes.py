4f8372a45a3919b08e22be8171484baf399058a9
steeve0403 <zychsteeve4@gmail.com>
2024-10-08 12:00:57 +0200

    logger configuration and routes

--- src/routes.py
+++ src/routes.py
@@ -1,26 +1,40 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from src.controllers.user_controller import signup, login, logout, generate_api_key
 from src.middlewares.decorators import role_required
 from flask_jwt_extended import jwt_required
+import logging
+
+# Logger configuration
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 auth_bp = Blueprint('auth', __name__)
 
 # Route for registration
 auth_bp.route('/signup', methods=['POST'])(signup)
 
-# Route for connexion
+# Route for login
 auth_bp.route('/signin', methods=['POST'])(login)
 
 # Route for logout
 auth_bp.route('/logout', methods=['POST'])(logout)
 
 # Route for generating an API key
-auth_bp.route('generate', methods=['POST'])(generate_api_key)
+auth_bp.route('/generate', methods=['POST'])(generate_api_key)
 
-
-# Route réservée aux administrateurs
+# Route reserved for administrators
 @auth_bp.route('/admin/dashboard', methods=['GET'])
 @jwt_required()
-@role_required('admin')  # Vérification du rôle admin avant d'autoriser l'accès
+@role_required('admin')  # Verify admin role before allowing access
 def admin_dashboard():
-    return admin_dashboard()
+    """
+    Admin dashboard endpoint (placeholder).
+
+    :return: JSON response indicating success.
+    """
+    try:
+        logger.info("Admin dashboard accessed")
+        return jsonify({"message": "Welcome to the admin dashboard"}), 200
+    except Exception as e:
+        logger.error(f"Error accessing admin dashboard: {str(e)}")
+        return jsonify({'status': 'failed', 'message': 'An error occurred', 'error': str(e)}), 500
