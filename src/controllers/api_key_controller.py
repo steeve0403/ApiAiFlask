91ccb7dc3dd8ad0adfdefe1758d1c91671415735
steeve0403 <zychsteeve4@gmail.com>
2024-10-12 11:36:59 +0200

    Central mechanism for processing errors and custom exceptions for better clarity and granularity of errors:

--- src/controllers/api_key_controller.py
+++ src/controllers/api_key_controller.py
@@ -1,11 +1,13 @@
 from flask import jsonify
 from flask_jwt_extended import jwt_required
 from src.services.api_key_service import generate_api_key_service, get_user_api_keys_service
+from src.exceptions import AppErrorBaseClass, ValidationError
 import logging
 
 # Logger configuration
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
+
 
 @jwt_required()
 def generate_api_key():
@@ -17,9 +19,13 @@ def generate_api_key():
     try:
         response = generate_api_key_service()
         return jsonify(response), 201
-    except Exception as e:
+    except AppErrorBaseClass as e:
         logger.error(f"Error generating API key: {str(e)}")
-        return jsonify({'status': 'failed', 'message': 'Error generating API key', 'error': str(e)}), 500
+        return jsonify({'status': 'failed', 'message': str(e)}), 400
+    except Exception as e:
+        logger.error(f"Unexpected error generating API key: {str(e)}")
+        return jsonify({'status': 'failed', 'message': 'An unexpected error occurred while generating the API key',
+                        'error': str(e)}), 500
 
 
 @jwt_required()
@@ -32,6 +38,10 @@ def get_user_api_keys():
     try:
         response = get_user_api_keys_service()
         return jsonify(response), 200
-    except Exception as e:
+    except AppErrorBaseClass as e:
         logger.error(f"Error retrieving API keys: {str(e)}")
-        return jsonify({'status': 'failed', 'message': 'Error retrieving API keys', 'error': str(e)}), 500
+        return jsonify({'status': 'failed', 'message': str(e)}), 400
+    except Exception as e:
+        logger.error(f"Unexpected error retrieving API keys: {str(e)}")
+        return jsonify({'status': 'failed', 'message': 'An unexpected error occurred while retrieving the API keys',
+                        'error': str(e)}), 500
